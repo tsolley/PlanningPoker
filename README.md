b42360679b5df7604188c4328d7e074fb3f97104
Trevor Lewis <tlewis@xpanxion.com>
2016-01-22 15:29:36 -0700

    Project structure and setup update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # PlanningPoker
+
+I would suggest using the Spring Tool Suite as an IDE. STS has great Gradle support. Plus I made the project a Spring Boot project but it is configured to run from a local Tomcat.
---
+++ src/main/java/com/planning/poker/PlanningPokerApplication.java
@@ -0,0 +1,12 @@
+package com.planning.poker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningPokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningPokerApplication.class, args);
+	}
+}
